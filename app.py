904d66eba1188795405df85230ec7a7f7da5b904
MiskoEgle <silinskyte@gmail.com>
2025-02-25 23:50:09 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 # app.py
 
-import Classes.vartotojai as vart
-import Functions.vartotojo_registracija as reg
-import Functions.filmu_registracija as fil_reg
-import Functions.filmu_paieska as paieska
+# import Classes.vartotojai as vart
+# import Functions.vartotojo_registracija as reg
+# import Functions.filmu_registracija as fil_reg
+# import Functions.filmu_paieska as paieska
 
 
 
-if __name__ == "_main_":
-    # reg.vartotojo_meniu()
-    # fil_reg.filmu_restravimo_meniu()
-    paieska.filmu_perziura_ir_paieska()
+# if __name__ == "__main__":
+#     try:
+#         reg.vartotojo_meniu()
+#         fil_reg.filmu_restravimo_meniu()
+#         paieska.filmu_perziura_ir_paieska()
+#     except Exception as e:
+#         print(f"Klaida: {e}")
     
+
+import Functions.vartotojo_registracija as regist
+
+if __name__== "_main_":
+    regist.vartotojo_meniu()
+
---
+++ config.py
@@ -0,0 +1,53 @@
+# config.py
+
+import Functions.filmu_registracija as fil_reg
+import Functions.filmu_paieska as paieska
+import Functions.filmu_reitingavimas as reiting
+import Functions.seansu_planavimas as seans
+import Functions.bilietu_valdymas as bilietai
+
+def svecio_meniu():
+    while True:
+        print("Pasirinkite norimą funkciją")
+        print("1. Filmų peržiūra ir paieška")
+        print("2. Bilietų rezervacija")
+        print("3. Filmų reitingavimas")
+        print("4. Išeiti")
+        
+        pasirinkimas = input("Pasirinkite veiksmą: ")
+            
+        if pasirinkimas == '1':
+            paieska.filmu_perziura_ir_paieska()
+        elif pasirinkimas == '2':
+            bilietai.bilietai_meniu()
+        elif pasirinkimas == '3':
+            reiting.filmu_reitingavimas()
+        elif pasirinkimas == '4':
+            print("\nSvečias atsijungė\n")
+            break
+        else:
+            print("Neteisingas pasirinkimas. Bandykite dar kartą.")
+    
+def organizatoriu_meniu():
+    while True:
+        print("Pasirinkite norimą funkciją")
+        print("1. Filmų registracija")
+        print("2. Filmų peržiūra ir paieška")
+        print("3. Seansų planavimas")
+        print("4. Išeiti")
+        
+        pasirinkimas = input("Pasirinkite veiksmą: ")
+            
+        if pasirinkimas == '1':
+            fil_reg.filmu_registravimo_meniu()
+        elif pasirinkimas == '2':
+            paieska.filmu_perziura_ir_paieska()
+        elif pasirinkimas == '3':
+            seans.seanso_meniu()
+        elif pasirinkimas == '4':
+            print("\nOrganizatorius atsijungė\n")
+            break
+        else:
+            print("Neteisingas pasirinkimas. Bandykite dar kartą.")
+            
+
